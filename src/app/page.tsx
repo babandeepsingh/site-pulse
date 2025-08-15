51e2673eae65fc067ea7bd0f6bb6ccdac6761038
Babandeep Singh <babandeepsingh@gmail.com>
2025-08-15 23:45:39 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { useUser } from "@clerk/nextjs";
+import { SignIn } from "@clerk/clerk-react";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,7 +9,8 @@ export default function Home() {
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {isSignedIn && <h6>Welcome, {user?.fullName}</h6>}
+      {isSignedIn ? <><UserButton /> <h6>Welcome, {user?.fullName}</h6></> : <SignIn routing="hash" />
+      }
 
     </div>
   );
